9657b6cc066be552d8ee638ed8a628623c719fad
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-24 00:24:18 +0200

    Aggiunto parti milestone 5

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var app = new Vue(
             contactIndex: 0,
             contactFilter: '',
             userMessage: '',
+            
             contacts: [
                 {
                     name: 'Michele',
@@ -14,17 +15,20 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            dropdown: false
                         }
                     ],
                 },
@@ -36,17 +40,20 @@ var app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             text: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
-                        },
+                            status: 'received',
+                            dropdown: false
+                        },    
                         {
                             date: '20/03/2020 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         }
                     ],
                 },
@@ -58,17 +65,20 @@ var app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             text: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            dropdown: false
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             text: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            dropdown: false
                         }
                     ],
                 },
@@ -80,12 +90,14 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdown: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            dropdown: false
                         }
                     ],
                 },
@@ -114,7 +126,8 @@ var app = new Vue(
                 const userMessageItem = {
                     date : dayjs().format("DD/MM/YYYY HH:mm:ss"),
                     text : this.userMessage,
-                    status : 'sent'
+                    status : 'sent',
+                    dropdown: false
                 }
 
                 if( this.userMessage.length != '') {
@@ -131,11 +144,22 @@ var app = new Vue(
                     const botMessage = {
                         date : dayjs().format("DD/MM/YYYY HH:mm:ss"),
                         text : 'Hola',
-                        status : 'received'
+                        status : 'received',
+                        dropdown: false
                     }
 
                     this.contacts[index].messages.push(botMessage);
                 }, 1000 ); 
+            },
+            showDropDown(index) {
+                let mesIndex = index;
+                this.contacts[this.contactIndex].messages.forEach((element, index) => {
+                    if( mesIndex == index ) {
+                        element.dropdown = !element.dropdown;
+                    } else {
+                        element.dropdown = false;
+                    }
+                });  
             }
         }
     }
